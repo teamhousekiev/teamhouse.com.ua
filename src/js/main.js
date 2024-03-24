ce63a0f36a8b0f82c0694d22be463161bd476105
unknown <teamhousekiev@gmail.com>
2024-03-24 18:01:36 +0200

    Title change

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ const btn = document.querySelector(".header-list__btn");
 const menu = document.querySelector(".header-list");
 const item = document.getElementsByClassName("header-list__item");
 
+let newPath = window.location.href.replace(window.location.pathname, '/teamhouse/');
+window.history.replaceState({}, document.title, newPath);
+
 btn.addEventListener("click",function(){
     menu.classList.toggle("active");
     btn.classList.toggle("active");
