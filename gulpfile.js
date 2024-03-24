19b169f6048cbeed4131726d1a0e5656684609d8
unknown <teamhousekiev@gmail.com>
2024-03-24 17:54:13 +0200

    change title

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
-const { series,task, parallel } = require("gulp");
+const { series, task, parallel, src, dest } = require("gulp");
 const { clean } = require("./gulp-tasks/clean");
-const { styles } = require("./gulp-tasks/style");
 const { scripts } = require("./gulp-tasks/script");
 const { images } = require("./gulp-tasks/img");
 
-task("build",series(clean,scripts,styles,images));
+// Виправлення для правильного імпорту пакету gulp-sass
+const sass = require('gulp-sass')(require('sass'));
+
+// Определяем задачу styles з використанням sass компілятора
+task("styles", function () {
+  return src('src/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/css'));
+});
+
+task("build", series(clean, scripts, 'styles', images)); // Виправлення виклику styles
 
 const { serv } = require("./gulp-tasks/serv");
 const { watch } = require("./gulp-tasks/watch");
 
-task("dev",parallel(watch,serv));
-
-
+task("dev", parallel(watch, serv));
 
